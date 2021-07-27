5be8cd2c6745ab7c8bd780af854a89281682b28b
Jorge Lucas <jlucaspaixao@outlook.com>
2021-07-27 00:33:21 -0300

    WIP: Implementando Prismic

--- src/components/Brands/index.tsx
+++ src/components/Brands/index.tsx
@@ -1,7 +1,27 @@
 import { Box, Container } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Brands = () => {
+  const [brands, setBrands] = useState([]);
+
+  const getData = () => {
+    fetch(`https://toti-academy.cdn.prismic.io/api/v2/documents/search?ref=YP93ABAAACQAbaV0&type=brands`, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (myJson) {
+        console.log(myJson);
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <Box bg="black" py="90px">
       <Container>marcas</Container>
